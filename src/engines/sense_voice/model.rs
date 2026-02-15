339fdaa489325068df5803d4858ad0fb5dddffdb
Andrew Leech <andrew.leech@planetinnovation.com.au>
2026-02-16 07:29:29 +1100

    bump ort to rc.11

--- src/engines/sense_voice/model.rs
+++ src/engines/sense_voice/model.rs
@@ -95,7 +95,7 @@ impl SenseVoiceModel {
         log::info!("Loading SenseVoice model from {:?}...", model_path);
         let session = Self::init_session(&model_path)?;
 
-        let input_names: Vec<String> = session.inputs.iter().map(|i| i.name.clone()).collect();
+        let input_names: Vec<String> = session.inputs().iter().map(|i| i.name.clone()).collect();
         log::debug!("Model inputs: {:?}", input_names);
 
         let metadata = Self::parse_metadata(&session)?;
@@ -130,19 +130,11 @@ impl SenseVoiceModel {
             .with_parallel_execution(true)?
             .commit_from_file(path)?;
 
-        for input in &session.inputs {
-            log::info!(
-                "Model input: name={}, type={:?}",
-                input.name,
-                input.input_type
-            );
+        for input in session.inputs() {
+            log::info!("Model input: name={}, type={:?}", input.name(), input.dtype());
         }
-        for output in &session.outputs {
-            log::info!(
-                "Model output: name={}, type={:?}",
-                output.name,
-                output.output_type
-            );
+        for output in session.outputs() {
+            log::info!("Model output: name={}, type={:?}", output.name(), output.dtype());
         }
 
         Ok(session)
